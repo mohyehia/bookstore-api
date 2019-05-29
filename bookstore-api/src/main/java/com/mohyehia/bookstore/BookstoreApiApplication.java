6afaeca65fb328607a93f4e81539006c1dcdf96e
mohyehia <mohammedyehia99@gmail.com>
2019-05-29 18:29:40 +0200

    Initial commit

---
+++ bookstore-api/src/main/java/com/mohyehia/bookstore/BookstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.mohyehia.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApiApplication.class, args);
+	}
+
+}
